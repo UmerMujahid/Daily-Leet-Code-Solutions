d1f1cceaab4bb49c6329a8c2b47c756b93e1744b
Umer Mujahid <umermujahid4738@gmail.com>
2026-06-18 13:28:57 +0500

    Add Search Insert method for binary search insertion
    Implement binary search to find the insert position for a target in a sorted list.

---
+++ 35. Search Insert Position.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start =0
+        end=len(nums)-1
+
+        if len(nums)==0:
+            return 0
+
+        if nums[0]==target:
+            return 0
+
+        while(start<=end):
+            mid=(start+end)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>=target:
+                end=mid-1
+            else:
+                start=mid+1
+        
+        return start
+        
