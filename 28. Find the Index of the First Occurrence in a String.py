bcea6ae31395b8d575980d233a74e3bb1fbb1bb4
Umer Mujahid <umermujahid4738@gmail.com>
2026-07-18 02:33:49 +0500

    Implement strStr function to find substring index

---
+++ 28. Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        pos=haystack.find(needle)
+        return pos
+        
